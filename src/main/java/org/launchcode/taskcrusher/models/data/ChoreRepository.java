7bb4689edc283169f226b2f44660edab853bdec6
bhavana1308 <bonamukkala.bhavana@gmail.com>
2024-01-19 08:17:48 -0600

    created model classes for chores

--- src/main/java/org/launchcode/taskcrusher/models/data/ChoreRepository.java
+++ src/main/java/org/launchcode/taskcrusher/models/data/ChoreRepository.java
@@ -1,10 +1,10 @@
 package org.launchcode.taskcrusher.models.data;
 
 import org.launchcode.taskcrusher.models.Chore;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ChoreRepository extends JpaRepository<Chore, Integer> {
+public interface ChoreRepository extends CrudRepository<Chore, Integer> {
 
 }
