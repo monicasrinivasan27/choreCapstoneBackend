1fede01de3016d7d954f3d74919aa79de5d563bb
monicasrinivasan27 <srinivasan.monica@gmail.com>
2024-01-23 17:05:23 -0600

    Created an basic execution of API

--- src/main/java/org/launchcode/taskcrusher/controllers/ApiController.java
+++ src/main/java/org/launchcode/taskcrusher/controllers/ApiController.java
@@ -1,9 +1,25 @@
 package org.launchcode.taskcrusher.controllers;
 
+import org.launchcode.taskcrusher.service.ApiService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class ApiController {
+    private final ApiService apiService;
+
+     @GetMapping("/holiday-data")
+    public ResponseEntity<?> endpointCallApi() {
+        return ResponseEntity.ok(apiService.getAllHoliday());
+    }
+
+    public ApiController(ApiService apiService) {
+        this.apiService = apiService;
+    }
+
+
 
 }
--- src/main/java/org/launchcode/taskcrusher/service/ApiService.java
+++ src/main/java/org/launchcode/taskcrusher/service/ApiService.java
@@ -1,7 +1,33 @@
 package org.launchcode.taskcrusher.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.logging.ErrorManager;
 
 @Service
 public class ApiService {
+    private static final String url = "https://date.nager.at/api/v3/publicholidays";
+    private static final String countryCode = "US";
+    private static final String yearCode = "2024";
+    private static final String urlFull=url+"/"+yearCode+"/"+countryCode;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public Object getAllHoliday() {
+        try {
+            ResponseEntity<String> response = restTemplate.getForEntity(urlFull, String.class);
+            return response.getBody();
+        } catch (Exception errors) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error while calling the API", errors);
+        }
+
+    }
+
+
 }
