72b360d65ebb4fb0019d2b6d93697dc8c638f1ca
katytapley <katy.tapley@gmail.com>
2024-01-26 12:04:55 -0600

    Confused on what to do with return statements. Tried to adjust, but will likely need to go back and adjust

--- src/main/java/org/launchcode/taskcrusher/controllers/ParentAuthenticationController.java
+++ src/main/java/org/launchcode/taskcrusher/controllers/ParentAuthenticationController.java
@@ -28,7 +28,7 @@ public class ParentAuthenticationController {
 
     // This stores key/value pair with session key and parent user ID
     private static void setParentUserInSession(HttpSession session, ParentUser parentUser) {
-        session.setAttribute(parentUserSessionKey, parentUser.getId());
+        session.setAttribute(parentUserSessionKey, parentUser.getParentId());
         System.out.println("session: " + session.getAttribute("parentUser"));
         // The above line was in Carrie's lecture. Likely not needed?
     }
@@ -53,27 +53,27 @@ public class ParentAuthenticationController {
     }
 
     // Handlers for registration form
-    @GetMapping("/parent-register")
+    @GetMapping("/api/parent-register")
     public String displayParentRegistrationForm(Model model, HttpSession session){
         model.addAttribute(new ParentRegistrationFormDTO());
         model.addAttribute("loggedIn", session.getAttribute("parentUser") != null);
-        return "parent-register";
+        return "/api/parent-register";
     }
 
-    @PostMapping("/parent-register")
+    @PostMapping("/api/parent-register")
     public String processParentRegistrationForm(@ModelAttribute @Valid ParentRegistrationFormDTO parentRegistrationFormDTO,
                                                 Errors errors,
                                                 HttpServletRequest request) {
         // Sends parent user back to form if errors are found
         if (errors.hasErrors()) {
-            return "parent-register";
+            return "Please fill all fields in correctly";
         }
         //Look up parent user in the DB using the username they provided in the form
         ParentUser existingParentUser = parentUserRepository.findByUsername(parentRegistrationFormDTO.getUsername());
         // Sends parent user back to form if username already esists
         if (existingParentUser != null) {
             errors.rejectValue("username", "username.alreadyExists", "A user with that username already exists");
-            return "parent-register";
+            return "Username already exists";
         }
 
         // ADDED 1/23 ---------------------------------------
@@ -81,7 +81,7 @@ public class ParentAuthenticationController {
         ParentUser existingParentEmail = parentUserRepository.findByEmail(parentRegistrationFormDTO.getEmail());
         if (existingParentEmail != null) {
             errors.rejectValue("email", "email.alreadyExists", "An account is already created with that email");
-            return "parent-register";
+            return "There is already an account linked to that email address";
         }
         // ADDED 1/23 ---------------------------------------
 
@@ -90,7 +90,7 @@ public class ParentAuthenticationController {
         String verifyPassword = parentRegistrationFormDTO.getVerifyPassword();
         if (!password.equals(verifyPassword)) {
             errors.rejectValue("password", "passwords.mismatch", "Passwords do not match");
-            return "parent-register";
+            return "Passwords do not match";
         }
         //If there are no errors, save new username and hashed password in DB, start a new session, and redirect to
         // parent homepage
@@ -98,24 +98,24 @@ public class ParentAuthenticationController {
                 parentRegistrationFormDTO.getPassword());
         parentUserRepository.save(newParentUser);
         setParentUserInSession(request.getSession(), newParentUser);
-        return "redirect:/parent-dash";
+        return "redirect:/api/parent-dash";
     }
 
     //Handlers for login form
-    @GetMapping("/parent-login")
+    @GetMapping("/api/parent-login")
     public String displayParentLoginForm(Model model, HttpSession session) {
         model.addAttribute(new ParentLoginFormDTO());
         model.addAttribute("loggedIn", session.getAttribute("parentUser") != null);
-        return "api/parent-login";
+        return "/api/parent-login";
     }
 
-    @PostMapping("/parent-login")
+    @PostMapping("/api/parent-login")
     public String processParentLoginForm(@ModelAttribute @Valid ParentLoginFormDTO parentLoginFormDTO,
                                          Errors errors,
                                          HttpServletRequest request) {
         // This sends parent user back to the form if errors are found
         if (errors.hasErrors()) {
-            return "parent-login";
+            return "/api/parent-login";
         }
         // This looks up parent user in DB using username they provided in the form
         ParentUser theParentUser = parentUserRepository.findByUsername(parentLoginFormDTO.getUsername());
@@ -125,17 +125,17 @@ public class ParentAuthenticationController {
         if (theParentUser == null || !theParentUser.isMatchingPassword(parentPassword)) {
             errors.rejectValue("password", "login.invalid", "Credentials invalid. Please try again with correct " +
                     "username and/or password");
-            return "parent-login";
+            return "/api/parent-login";
         }
         // If there are no errors, create a new session for the parent user and direct them to parent dashboard
         setParentUserInSession(request.getSession(), theParentUser);
-        return "redirect:/parent-dash";
+        return "redirect:/api/parent-dash";
     }
 
     //Handler for logging out
-    @GetMapping("/parent-logout")
+    @GetMapping("/api/parent-logout")
     public String parentLogout(HttpServletRequest request) {
         request.getSession().invalidate();
-        return "redirect:/parent-login";
+        return "redirect:/api/parent-login";
     }
 }
