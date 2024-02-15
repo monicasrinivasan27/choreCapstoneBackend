569925d21a643292c378ce25f6bf931f268f28d6
bhavana1308 <bonamukkala.bhavana@gmail.com>
2024-02-15 13:48:19 -0600

    code cleanup

--- src/main/java/org/launchcode/taskcrusher/configure/SecurityConfig.java
+++ src/main/java/org/launchcode/taskcrusher/configure/SecurityConfig.java
@@ -21,29 +21,6 @@ public class SecurityConfig {
     private final UserAuthenticationEntryPoint userAuthenticationEntryPoint;
     private final UserAuthProvider userAuthProvider;
 
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                .exceptionHandling(customizer -> customizer.authenticationEntryPoint(userAuthenticationEntryPoint))
-//                .addFilterBefore(new JwtAuthFilter(userAuthProvider), BasicAuthenticationFilter.class)
-//                .csrf(AbstractHttpConfigurer::disable)
-//                .sessionManagement(customizer -> customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//                .authorizeHttpRequests((requests) -> requests
-//                      //  .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-//                        .requestMatchers(HttpMethod.POST,  "/api/parentLogin", "/api/register",
-//                                "/api/kidLogin").permitAll()//<-ONLY endpoints where auth is not required
-//                        .requestMatchers(HttpMethod.GET,"/api/chores/list","/api/parent-dashboard/statistics", "/api/chores/edit/{choreId}","/api/assignments/kids").permitAll()
-//                        .requestMatchers(HttpMethod.POST,"/api/chores/add","/api/assignments/assign-chore" ).permitAll()
-//                        .requestMatchers(HttpMethod.PUT,"/api/chores/edit/{choreId}" ).permitAll()
-//                        .requestMatchers(HttpMethod.DELETE,"/api/chores/{choreId}" ).permitAll()
-//                        .anyRequest().authenticated()
-//
-//                );
-//
-//        return http.build();
-//
-//    }
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
