fdd7e3f7f393d283702cb3586a537dd2755bcd71
bhavana1308 <bonamukkala.bhavana@gmail.com>
2024-03-02 13:58:15 -0600

    minor change

--- src/main/java/org/launchcode/taskcrusher/models/dto/KidUserDto.java
+++ src/main/java/org/launchcode/taskcrusher/models/dto/KidUserDto.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 public class KidUserDto {
 
     private int id;
-    private String firstName;
+    private String name;
     private String username;
     private String token;
 
